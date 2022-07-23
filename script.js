7093a6d4eb22a4832b95a4b0d2192a2a0bebec17
mak <vlad.avtomat@mail.ru>
2022-07-23 11:39:30 +0300

    functional add

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function randomInteger() {
+let renderAnsw = document.getElementById("trOrFl");
+
+function render() {
+    let inp = document.getElementById("inp").value;
     let rand = 1 - 0.5 + Math.random() * (5 - 1 + 1);
-    return Math.round(rand);
+    let randoms = Math.round(rand);
+    if (inp == randoms) {
+        renderAnsw.innerHTML = `You trust! Robot says ${randoms}`;
+    } else {
+        renderAnsw.innerHTML = `Not that. Wrong number is ${randoms}`;
+    }
 }
 
-let
+document.getElementById("checkButton").onclick = render;
